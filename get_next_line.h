8eec699962b3afa3d4eb03098e236f1f54228481
Jiyan Wang <jiyawang@1-D-18.42heilbronn.de>
2025-08-03 13:48:36 +0200

    updateat42

--- get_next_line.h
+++ get_next_line.h
@@ -6,21 +6,19 @@
 /*   By: jiyawang <jiyawang@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/08/01 20:14:28 by jiyawang          #+#    #+#             */
-/*   Updated: 2025/08/01 20:14:31 by jiyawang         ###   ########.fr       */
+/*   Updated: 2025/08/03 12:56:46 by jiyawang         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
 
+# include <fcntl.h>
 # include <stdlib.h>
 # include <unistd.h>
-# include <fcntl.h>
 
-
-char *get_next_line(int fd);
-int open (const char* path, int flags, int mode);
-ssize_t read(int fildes, void *buf, size_t nbyte);
-
+char	*get_next_line(int fd);
+int		open(const char *path, int flags, int mode);
+ssize_t	read(int fildes, void *buf, size_t nbyte);
 
 #endif
