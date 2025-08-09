0b90a8dcdb387a55e1bad47b871e53c149d7e4f3
Fiona-87327 <jiyanw825@gmail.com>
2025-08-09 15:59:08 +0200

    update

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -24,20 +24,17 @@ static size_t	ft_strlen(const char *str)
 	return (len);
 }
 
-static char	*ft_strrchr(const char *s, int c)
+static char	*ft_strchr(const char *s, int c)
 {
-	unsigned int	i;
-	char			*last;
-
-	i = 0;
-	last = 0;
-	while (s[i])
+	if (!*s)
+		return (NULL);
+	while (*s)
 	{
-		if (s[i] == (char)c)
-			last = (char *)&s[i];
-		i++;
+		if (*s == (char)c)
+			return ((char *)s);
+		s++;
 	}
-	if (s[i] == (char)c)
-		last = (char *)&s[i];
-	return (last);
+	if (c == '\0')
+		return ((char *)s);
+	return (NULL);
 }
