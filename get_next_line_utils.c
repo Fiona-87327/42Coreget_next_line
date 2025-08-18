6a2658d6599f366d200c3d94308ccd97719ecfba
Fiona-87327 <jiyanw825@gmail.com>
2025-08-18 21:37:03 +0200

    updat0818

--- get_next_line_utils.c
+++ get_next_line_utils.c
@@ -11,7 +11,22 @@
 /* ************************************************************************** */
 
 #include "get_next_line.h"
-
+char	*ft_strdup(char *s1)
+{
+	char	*dest;
+	unsigned int	i;
+	dest = (char *)malloc(ft_strlen(s1) + 1);
+	if (!dest)
+		return (NULL);
+	i = 0;
+	while (s1[i])
+	{
+		dest[i] = s1[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+}
 size_t	ft_strlen(const char *str)
 {
 	size_t	len;
@@ -22,6 +37,32 @@ size_t	ft_strlen(const char *str)
 	while (str[len] != '\0')
 		len++;
 	return (len);
+}
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	size_t	i;
+	size_t 	j;
+	size_t	s_len;
+	char	*subs;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	i = 0;
+	while (i < len && start < s_len && s[start + i])
+		i++;
+	subs = (char *)malloc(i + 1);
+	if (!subs)
+		return (NULL);
+	j = 0;
+	while (j < i)
+	{
+		subs[j] = s[start + j];
+		j++;
+	}
+	subs[i] = '\0';
+	return (subs);
 }
 
 char	*ft_strchr(const char *s, int c)
@@ -51,15 +92,16 @@ char	*ft_strjoin(char *s1, char *s2)
 		s1_len = ft_strlen(s1);
 	if (s2)
 		s2_len = ft_strlen(s2);
-	result = malloc(s1_len + s2_len + 1);
+	result = (char *)malloc(s1_len + s2_len + 1);
 	if (!result)
-		return (NULL);
-	i = -1;
-	while (s1 && s1[i])
 	{
-		result[i] = s1[i];
-		i++;
+		free(s1);
+		return (NULL);
 	}
+	i = 0;
+	j = 0;
+	while (s1 && s1[j])
+		result[i++] = s1[j++];
 	j = 0;
 	while (s2 && s2[j])
 		result[i++] = s2[j++];
